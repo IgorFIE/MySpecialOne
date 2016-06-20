9f6ab943f2ce4faaca130e4d84a621e7db943566
Nelson Oliveira <nelsonfbo@gmail.com>
2016-06-20 19:39:47 +0100

    Update ClientConnection.java with input/output streams & methods

--- NetworkGame/src/ClientConnection.java
+++ NetworkGame/src/ClientConnection.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
@@ -5,14 +9,56 @@ import java.net.Socket;
  */
 public class ClientConnection implements Runnable{
 
-    private Socket clientSocker;
+    private Socket clientSocket;
 
-    public ClientConnection(Socket clientSocket){
-        this.clientSocker = clientSocket;
-    }
+    private BufferedReader in = null;
+    private PrintWriter out = null;
+
+    private Server myServer;
+    private String myName;
+
+    /**
+     * Constructor
+     * @param clientSocket - Socket for player connection to the server.
+     * @param server - The server that controls and distributes player updates.
+     */
+    public ClientConnection(Socket clientSocket, Server server){ this.clientSocket = clientSocket; this.myServer = server;}
 
     public void run() {
-        //in
-        //out
+
+        try {
+
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            while (true){
+
+                String inputStream = in.readLine();
+
+                // TODO - SOUT for debugging purposes only, DELETE this after.
+                System.out.println(inputStream);
+
+                //TODO - Implement proper broadcasting method from Server.
+                //myServer.sendAll(inputStream);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error with Input/Output Steams"+e.getMessage());
+        }
+
     }
+
+
+    public void send(String message){
+        out.write(message + "\n");
+        out.flush();
+    }
+
+    //SETTER - For custom client name (if needed)
+    public void setName(String newName){
+        myName = newName;
+    }
+
 }
+
+
