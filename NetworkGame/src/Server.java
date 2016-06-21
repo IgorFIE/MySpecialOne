9b8e1576b7e74429ab4789e8d83e5e7b21c1f414
Sofia <ana.smirv@gmail.com>
2016-06-21 09:50:16 +0100

    with lanterna second

--- NetworkGame/src/Server.java
+++ NetworkGame/src/Server.java
@@ -1,5 +1,7 @@
 import java.net.ServerSocket;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by codecadet on 20/06/16.
@@ -8,7 +10,7 @@ public class Server {
 
     private final int portNumber = 8080;
     private ServerSocket serverSocket = null;
-    private List<ClientConnection> clientList = Collections.synchronizedList(new LinkedList<>());
+    private List<ClientConnection> clientList = Collections.synchronizedList(new LinkedList());
     //pool
 
     public Server(){
