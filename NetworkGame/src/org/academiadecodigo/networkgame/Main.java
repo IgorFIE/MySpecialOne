c6d2dc96c1fb4556e042227ccd33afecdccda4f1
Igor Estevao <IgorFIE@hotmail.com>
2016-06-26 20:27:18 +0100

    Main comments

--- NetworkGame/src/org/academiadecodigo/networkgame/Main.java
+++ NetworkGame/src/org/academiadecodigo/networkgame/Main.java
@@ -8,11 +8,18 @@ import org.academiadecodigo.networkgame.Server.UDPServer;
  */
 public class Main {
     public static void main(String[] args) {
+
+        /**
+         * if the first argument is server it will create a server with the second argument passed
+         */
         if(args[0].equals("server")){
             UDPServer server = new UDPServer(Integer.parseInt(args[1]));
             server.startServer();
         }
 
+        /**
+         * if the first argument is game it will create a game that will connect to a respective number and port
+         */
         if(args[0].equals("game")){
             UDPGame game = new UDPGame(args[1], Integer.parseInt(args[2]));
             game.start();
