78c52ebd7854c62a05a2c46cfa37f5051d46eede
Igor Estevao <IgorFIE@hotmail.com>
2016-06-26 18:56:19 +0100

    Project reorganize\server and game args

---
+++ NetworkGame/src/org/academiadecodigo/networkgame/Main.java
@@ -0,0 +1,21 @@
+package org.academiadecodigo.networkgame;
+
+import org.academiadecodigo.networkgame.Game.UDPGame;
+import org.academiadecodigo.networkgame.Server.UDPServer;
+
+/**
+ * Created by codecadet on 26/06/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        if(args[0].equals("server")){
+            UDPServer server = new UDPServer(args[1], Integer.parseInt(args[2]));
+            server.startServer();
+        }
+
+        if(args[0].equals("game")){
+            UDPGame game = new UDPGame(args[1]);
+            game.start();
+        }
+    }
+}
